9934e1d18565d8ae1ac77d9f35049268c0c11aa8
sanguedemonstro <sanguedemonstro@gmail.com>
2020-03-04 15:35:00 -0300

    1757323 - [LGPD] DEFINIR, IMPLEMENTAR E DOCUMENTAR COMO CONFIGURAR O WES PARA ENVIAR DADOS PARA O FLUENTD

---
+++ Benner.LGPDRepository/DefaultFileConfig.cs
@@ -0,0 +1,21 @@
+﻿using Benner.NoSQLRepository.Interfaces;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace Benner.LGPDRepository
+{
+    public class DefaultFileConfig : INoSQLConfiguration
+    {
+        public Dictionary<string, string> Settings { get; set; }
+
+        public void LoadSettings()
+        {
+            Settings = new Dictionary<string, string>
+            {
+                { "fluentd:Host", ConfigurationManager.AppSettings["fluentd:Host"] },
+                { "fluentd:Port", ConfigurationManager.AppSettings["fluentd:Port"] },
+                { "fluentd:Tag", ConfigurationManager.AppSettings["fluentd:Tag"] },
+            };
+        }
+    }
+}
