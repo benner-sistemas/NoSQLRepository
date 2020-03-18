b0c10bff666ea8aa8805ef6a52dca73c8d0f7666
leonardo.formaggi <leonardo.formaggi@benner.com.br>
2020-03-18 16:41:42 -0300

    Criada classe inicial para detalhes de log de relatorios

---
+++ Benner.LGPDRepository/Record/ReportDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace Benner.LGPD.Record
+{
+    public class ReportDetails : IRecordDetails
+    {
+        /// <summary>
+        /// Nome do relatório
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Código do relatório
+        /// </summary>
+        public string Codigo { get; set; }
+    }
+}
