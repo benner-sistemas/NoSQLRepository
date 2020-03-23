2c2efeb59ab4aef28a245c5cd970bb4becff0718
Nathan Kruger <nathan.kruger124@gmail.com>
2020-03-23 10:20:34 -0300

    Ajuste no enum do ILGPDConfiguration

---
+++ Benner.LGPDRepository/ILGPDConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Benner.NoSQLRepository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Benner.LGPD
+{
+
+    public interface ILGPDConfiguration : INoSQLConfiguration
+    {
+        TracerLevel Level { get; set; }
+    }
+    public enum TracerLevel
+    {
+        Disable,
+        Full
+    }
+}
