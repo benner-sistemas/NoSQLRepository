a303424843fa3c336decaa803c48fe7d463a97c9
Nathan Kruger <nathan.kruger124@gmail.com>
2020-04-03 15:47:17 -0300

    Removida a validação de configurações da API

--- Benner.LGPDRepository/FileConfig.cs
+++ Benner.LGPDRepository/FileConfig.cs
@@ -12,25 +12,12 @@ namespace Benner.LGPD
 
         public void LoadSettings()
         {
-            ValidaConfiguracoes();
             Settings = new Dictionary<string, string>
             {
                 { "fluentd:Host", ConfigurationManager.AppSettings["fluentd:Host"] },
                 { "fluentd:Port", ConfigurationManager.AppSettings["fluentd:Port"] },
                 { "fluentd:Tag", ConfigurationManager.AppSettings["fluentd:Tag"] },
             };
-        }
-
-        private void ValidaConfiguracoes()
-        {
-            if (string.IsNullOrEmpty(ConfigurationManager.AppSettings["fluentd:Host"]))
-                throw new InvalidOperationException("Host do fluentd deve ser informado");
-
-            if (string.IsNullOrEmpty(ConfigurationManager.AppSettings["fluentd:Port"]))
-                throw new InvalidOperationException("Port do fluentd deve ser informada");
-
-            if (string.IsNullOrEmpty(ConfigurationManager.AppSettings["fluentd:Tag"]))
-                throw new InvalidOperationException("Tag do fluentd deve ser informada");
         }
     }
 }
